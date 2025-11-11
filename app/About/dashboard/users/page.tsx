62c12b1a0f7506a3bfc663f1a9da298fbb63826c
Paul Obiero <157304888+DevvObiero@users.noreply.github.com>
2025-11-11 11:06:37 +0300

    react routing

--- app/About/dashboard/users/page.tsx
+++ app/About/dashboard/users/page.tsx
@@ -3,7 +3,11 @@ import React from 'react'
 const Users = () => {
   return (
     <div>
-      
+      <h2>DASHBOARD USERS</h2>
+      <ul>
+        <li className='mt-10 text-red-600 ' >User 1</li>
+
+      </ul>
     </div>
   )
 }
