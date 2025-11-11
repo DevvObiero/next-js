7588ef1fe8daea4939eee575d5dd8b83c3c9d7ca
Paul Obiero <157304888+DevvObiero@users.noreply.github.com>
2025-11-11 14:55:50 +0300

    don learning next js basics

--- app/dashboard/users/[id]/page.tsx
+++ app/dashboard/users/[id]/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 
-const UserDetails = () => {
+const UserDetails = async ({ params }: { params: Promise<{ id: string }> }) => {
+  const { id } = await params
+  
   return (
     <div>
-      <h2>DASHBOARD USERS</h2>
-
+      <h2>DASHBOARD USER {id}</h2>
+      <p>Details for user {id}</p>
     </div>
   )
 }
